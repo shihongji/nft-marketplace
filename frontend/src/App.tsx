8435a6c2869ada5a114b8152f08d73ab9a013c6b
Hongji <shihongji21@gmail.com>
2024-07-19 12:36:19 +0800

    frontend create collection (#3)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { WagmiProvider } from 'wagmi'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { config } from './wagmi'
-import Home from './pages/Home'
+import Home from './pages/Home.tsx'
 import '@rainbow-me/rainbowkit/styles.css'
 
 const queryClient = new QueryClient()
