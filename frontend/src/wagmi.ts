e0e8721171b8186aa2b599098f1423c62021afe8
Hongji <shihongji21@gmail.com>
2024-07-19 18:28:55 +0800

    unable to fix content security policy error (#5)

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,14 +1,15 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
-import { http } from 'wagmi'
-import { sepolia } from 'wagmi/chains'
-
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { http } from "wagmi";
+import { sepolia } from "wagmi/chains";
+const INFURA_API_KEY = "82a578dbf80b4b71b4289e6d907e7612";
 export const config = getDefaultConfig({
-  appName: 'NFT-marketplace',
-  projectId: '5d9583c04926c75b3cc048291bac62c7', // Get this from WalletConnect
+  appName: "NFT-marketplace",
+  projectId: "5d9583c04926c75b3cc048291bac62c7", // Get this from WalletConnect
   chains: [sepolia],
   transports: {
-    [sepolia.id]: http(),
+    [sepolia.id]: http(`https://sepolia.infura.io/v3/${INFURA_API_KEY}`),
   },
-})
+});
 
-export { sepolia }
+export { sepolia };
+
