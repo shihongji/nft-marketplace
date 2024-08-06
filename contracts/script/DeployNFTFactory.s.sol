73855162a995c27fdee6dd42b5e7448c5bbc4df4
MuLyu31 <123265801+MuLyu31@users.noreply.github.com>
2024-08-07 07:41:49 +0800

    Minor fix, and clean up (#8)

--- contracts/script/DeployNFTFactory.s.sol
+++ contracts/script/DeployNFTFactory.s.sol
@@ -1,25 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "forge-std/Script.sol";
-import "../src/NFTFactory.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Script} from "forge-std/Script.sol";
+import {NFTFactory} from "../src/NFTFactory.sol";
 
 contract DeployNFTFactory is Script {
-    function setUp() public {}
-
     function run() external returns (address) {
-        address proxy = deployV1();
-        return proxy;
-    }
-
-    function deployV1() public returns (address) {
         vm.startBroadcast();
-        NFTFactory nftFactory = new NFTFactory();
-        console.log("NFTFactory deployed to:", address(nftFactory));
-        ERC1967Proxy proxy = new ERC1967Proxy(address(nftFactory), "");
-        NFTFactory(address(proxy)).initialize();
+        NFTFactory factory = new NFTFactory();
+        factory.initialize(msg.sender);
         vm.stopBroadcast();
-        return address(proxy);
+        return address(factory);
     }
 }
